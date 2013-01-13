da037cb0303149696980fd48215cef1e7870d131
Paul Ser <pashka4281@gmail.com>
2013-01-13 09:50:23 +0200

    pinboard sidebar

--- source/javascripts/pinboard.js
+++ source/javascripts/pinboard.js
@@ -1,18 +1,48 @@
 function pinboardNS_fetch_script(url) {
   //document.writeln('<s'+'cript type="text/javascript" src="' + url + '"></s'+'cript>');
-  (function(){
+  
+  $(function(){
+    
     var pinboardLinkroll = document.createElement('script');
     pinboardLinkroll.type = 'text/javascript';
     pinboardLinkroll.async = true;
     pinboardLinkroll.src = url;
     document.getElementsByTagName('head')[0].appendChild(pinboardLinkroll);
-  })();
+
+    /*$.ajax({
+      url: url, 
+      dataType: 'script',
+      success: function(){
+        if(tag != '')
+          current_id = 'pinboard_' + tag;
+        else
+          current_id = 'pinboard_all';
+        console.log(current_id)
+      }
+    })*/
+
+  });
+
+
 }
 
-function pinboardNS_show_bmarks(r) {
+//callbacks
+function pinboard_show_all(r){
   var lr = new Pinboard_Linkroll();
   lr.set_items(r);
-  lr.show_bmarks();
+  lr.show_bmarks('pinboard_all');
+}
+
+function pinboard_show_by_tag1(r){
+  var lr = new Pinboard_Linkroll();
+  lr.set_items(r);
+  lr.show_bmarks('pinboard_' + pinboard_tag1);
+}
+
+function pinboard_show_by_tag2(r){
+  var lr = new Pinboard_Linkroll();
+  lr.set_items(r);
+  lr.show_bmarks('pinboard_' + pinboard_tag2);
 }
 
 function Pinboard_Linkroll() {
@@ -21,14 +51,14 @@ function Pinboard_Linkroll() {
   this.set_items = function(i) {
     this.items = i;
   }
-  this.show_bmarks = function() {
+  this.show_bmarks = function(id) {
     var lines = [];
     for (var i = 0; i < this.items.length; i++) {
       var item = this.items[i];
       var str = this.format_item(item);
       lines.push(str);
     }
-    document.getElementById(linkroll).innerHTML = lines.join("\n");
+    document.getElementById(id).innerHTML = lines.join("\n");
   }
   this.cook = function(v) {
     return v.replace('<', '&lt;').replace('>', '&gt>');
@@ -52,5 +82,12 @@ function Pinboard_Linkroll() {
   }
 }
 Pinboard_Linkroll.prototype = new Pinboard_Linkroll();
-pinboardNS_fetch_script("https://feeds.pinboard.in/json/v1/u:"+pinboard_user+"/?cb=pinboardNS_show_bmarks\&count="+pinboard_count);
+pinboardNS_fetch_script("https://feeds.pinboard.in/json/v1/u:"+pinboard_user+"/?cb=pinboard_show_all\&count="+pinboard_count);
 
+if(pinboard_tag1 != ''){
+  pinboardNS_fetch_script("https://feeds.pinboard.in/json/v1/u:"+pinboard_user+"/t:"+ pinboard_tag1 +"?cb=pinboard_show_by_tag1\&count="+pinboard_count);
+}
+
+if(pinboard_tag2 != ''){
+  pinboardNS_fetch_script("https://feeds.pinboard.in/json/v1/u:"+pinboard_user+"/t:"+ pinboard_tag2 +"?cb=pinboard_show_by_tag2\&count="+pinboard_count);
+}
